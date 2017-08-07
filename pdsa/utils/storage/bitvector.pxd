adec4da2d5fc493277bc4a5fb634e68a6836b97b
Andrii Gakhov <gakhov@ferret-go.com>
2017-08-07 14:16:23 +0200

    Use standard function to get quotient/remainder

--- pdsa/utils/storage/bitvector.pxd
+++ pdsa/utils/storage/bitvector.pxd
@@ -10,13 +10,9 @@ cdef extern from "src/BitField.h":
         void set_bit(uint8_t bit_number, bint flag)
         bint get_bit(uint8_t bit_number)
 
-cdef class BitPosition:
-    cdef size_t bucket
-    cdef uint8_t bit
 
 cdef class BitVector:
     cdef size_t size
     cdef size_t length
 
     cdef BitField * vector
-    cdef BitPosition _get_bit_position(self, const size_t index)
--- pdsa/utils/storage/bitvector.pyx
+++ pdsa/utils/storage/bitvector.pyx
@@ -3,14 +3,6 @@ from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
 cdef uint8_t BITFIELD_BITSIZE = sizeof(BitField) * 8
 
-cdef class BitPosition:
-    __slots__ = ()
-
-    def __cinit__(self, size_t bucket, uint8_t bit):
-        self.bucket = bucket
-        self.bit = bit
-
-
 cdef class BitVector:
 
     __slots__ = ()
@@ -24,36 +16,38 @@ cdef class BitVector:
         It's guaranteed that all bits in newly created structure will
         be cleared (set to 0).
         """
-        self.size = length // BITFIELD_BITSIZE
-        if length % BITFIELD_BITSIZE > 0:
-            self.size += 1
 
+        cdef size_t quotient
+        cdef int remainder
+
+        quotient, remainder = divmod(length, BITFIELD_BITSIZE)
+
+        self.size = quotient if remainder == 0 else quotient + 1
         self.length = self.size * BITFIELD_BITSIZE
 
         self.vector = <BitField *>PyMem_Malloc(self.size * sizeof(BitField))
         for bucket in range(self.size):
             self.vector[bucket].clear()
 
-    cdef BitPosition _get_bit_position(self, const size_t index):
-        """Calculate 2D position in the vector using flat index."""
-        cdef int bucket = index // BITFIELD_BITSIZE
-        cdef uint8_t bit = index % BITFIELD_BITSIZE
-
-        return BitPosition(bucket, bit)
-
     def __getitem__(self, const size_t index):
         if index > self.length:
             raise IndexError("Index {} out of range".format(index))
 
-        cdef BitPosition position = self._get_bit_position(index)
-        return self.vector[position.bucket].get_bit(position.bit)
+        cdef int bucket
+        cdef uint8_t bit
+
+        bucket, bit = divmod(index, BITFIELD_BITSIZE)
+        return self.vector[bucket].get_bit(bit)
 
     def __setitem__(self, const size_t index, const bint flag):
         if index > self.length:
             raise IndexError("Index {} out of range".format(index))
 
-        cdef BitPosition position = self._get_bit_position(index)
-        self.vector[position.bucket].set_bit(position.bit, flag)
+        cdef int bucket
+        cdef uint8_t bit
+
+        bucket, bit = divmod(index, BITFIELD_BITSIZE)
+        self.vector[bucket].set_bit(bit, flag)
 
     def __dealloc__(self):
         PyMem_Free(self.vector)
