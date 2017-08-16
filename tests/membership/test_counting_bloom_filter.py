fb50e1b03bf07b119328eb5fa4d91fe4864c7a69
Andrii Gakhov <andrii.gakhov@gmail.com>
2017-08-16 16:57:22 +0200

    Update test cases for Bloom filters

--- tests/membership/test_counting_bloom_filter.py
+++ tests/membership/test_counting_bloom_filter.py
@@ -5,14 +5,31 @@ from pdsa.membership.counting_bloom_filter import CountingBloomFilter
 
 def test_init():
     bf = CountingBloomFilter(8000, 3)
-
     assert bf.sizeof() == 5000, "Unexpected size in bytes"
+
+    with pytest.raises(ValueError) as excinfo:
+        bf = CountingBloomFilter(8000, 0)
+    assert str(excinfo.value) == 'At least one hash function is required'
+
+    with pytest.raises(ValueError) as excinfo:
+        bf = CountingBloomFilter(0, 5)
+    assert str(excinfo.value) == 'Filter length can\'t be 0 or negative'
 
 
 def test_init_from_capacity():
     bf = CountingBloomFilter.create_from_capacity(5000, 0.02)
-
     assert bf.sizeof() == 25445, "Unexpected size in bytes"
+
+    with pytest.raises(ValueError) as excinfo:
+        bf = CountingBloomFilter.create_from_capacity(5000, 2)
+    assert str(excinfo.value) == 'Error rate shell be in (0, 1)'
+
+    with pytest.raises(ValueError) as excinfo:
+        bf = CountingBloomFilter.create_from_capacity(0, 0.02)
+    assert str(excinfo.value) == 'Filter capacity can\'t be 0 or negative'
+
+    bf = CountingBloomFilter.create_from_capacity(5000, 0.999)
+    assert len(bf) == 16
 
 
 def test_repr():
