95fae4a84b33f78af46d2f1b5e1112bc138ad319
Andrii Gakhov <andrii.gakhov@gmail.com>
2017-08-16 17:38:10 +0200

    Update in-module documentation according to napoleon

--- pdsa/helpers/storage/bitvector_counter.pyx
+++ pdsa/helpers/storage/bitvector_counter.pyx
@@ -1,5 +1,4 @@
-"""
-BitVectorCounter.
+"""BitVectorCounter.
 
 BitVectorCounter is an array of C++ BitCounters (8 bits long, encoded
 two 4-bits counters) that supports flatten navigation.
@@ -14,21 +13,58 @@ cdef uint8_t NUMBER_OF_SUBCOUNTERS = 2
 
 
 cdef class BitVectorCounter:
+    """Implementation of a vector of 4-bits counters.
+
+    In fact, the bit vector is an array of C++ BitCounters (8 bits long)
+    that supports flatten navigation.
+
+    Example
+    -------
+
+    >>> from pdsa.helpers.storage.bitvector_counter import BitVector
+
+    >>> bv = BitVectorCounter(48)
+    >>> bv[37] = 1
+    >>> bv.increment(37)
+    >>> print(bv[37])
+    >>> bv.decrement(37)
+    >>> print(bv[37])
+
+
+    Attributes
+    ----------
+    length : :obj:`int`
+        The length of the vector's index space.
+    size : :obj:`int`
+        The size of the array of BitCounters.
+    vector : obj
+        The array of BitCounters.
+
+    """
 
     __slots__ = ()
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
-    # @cython.cdivision(True)
+    @cython.cdivision(True)
     def __cinit__(self, const size_t length):
-        """Allocate and initialize the vector of counters.
+        """Allocate and initialize a vector counters.
 
-        NOTE: each BitCounter includes 2 counters (4-bits), so we
-        the length can be rounded up to to closest even number.
+        Parameters
+        ----------
+        length : int
+            The length of the vector.
 
-        It's guaranteed that all counters in newly created structure will
-        be reset (set to 0).
+        Note
+        ----
+            It allocates space in blocks of 8 bits (byte),
+            that contain 2 (`NUMBER_OF_SUBCOUNTERS`) 4-bits counters,
+            therefore the length of the vector can be rounded up
+            to efficiently use the allocated memory.
+
         """
+        if length < 1:
+            raise ValueError("Length can't be 0 or negative")
 
         self.length = length + (-length & (NUMBER_OF_SUBCOUNTERS - 1))
         self.size = self.length // NUMBER_OF_SUBCOUNTERS
@@ -44,6 +80,19 @@ cdef class BitVectorCounter:
     @cython.wraparound(False)
     @cython.cdivision(True)
     def increment(self, const size_t index):
+        """Increment counter's value by the index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the counter in the vector.
+
+        Raises
+        ------
+        IndexError
+            If `index` is out of range.
+
+        """
         if index >= self.length:
             raise IndexError("Index {} out of range".format(index))
 
@@ -51,13 +100,26 @@ cdef class BitVectorCounter:
         cdef uint8_t counter_number
 
         bucket, counter_number = divmod(index, NUMBER_OF_SUBCOUNTERS)
-        return self.vector[bucket].inc(counter_number)
+        self.vector[bucket].inc(counter_number)
 
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
     @cython.cdivision(True)
     def decrement(self, const size_t index):
+        """Decrement counter's value by the index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the counter in the vector.
+
+        Raises
+        ------
+        IndexError
+            If `index` is out of range.
+
+        """
         if index >= self.length:
             raise IndexError("Index {} out of range".format(index))
 
@@ -65,13 +127,31 @@ cdef class BitVectorCounter:
         cdef uint8_t counter_number
 
         bucket, counter_number = divmod(index, NUMBER_OF_SUBCOUNTERS)
-        return self.vector[bucket].dec(counter_number)
+        self.vector[bucket].dec(counter_number)
 
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
     @cython.cdivision(True)
     def __getitem__(self, const size_t index):
+        """Get counter's value by the index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the counter in the vector.
+
+        Returns
+        -------
+        :obj:`int`
+            Counter's value.
+
+        Raises
+        ------
+        IndexError
+            If `index` is out of range.
+
+        """
         if index >= self.length:
             raise IndexError("Index {} out of range".format(index))
 
@@ -92,8 +172,24 @@ cdef class BitVectorCounter:
         )
 
     def __len__(self):
+        """Get length of the vector's index space.
+
+        Returns
+        -------
+        :obj:`int`
+            The length of the vector's index space.
+
+        """
         return self.length
 
     cpdef size_t sizeof(self):
+        """Size of the vector in bytes.
+
+        Returns
+        -------
+        :obj:`int`
+            Number of bytes allocated for the vector.
+
+        """
         return self.size * sizeof(BitCounter)
 
