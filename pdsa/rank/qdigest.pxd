100f62013847b79566ff8ab55f54bf4d505fc123
gakhov <andrii.gakhov@gmail.com>
2017-10-22 23:34:49 +0200

    Inefficient dirty implementation of compress/add funcs

--- pdsa/rank/qdigest.pxd
+++ pdsa/rank/qdigest.pxd
@@ -1,21 +1,27 @@
 from libc.stdint cimport uint32_t, uint8_t
 
 cdef class QuantileDigest:
-    cdef uint8_t _compression factor
+    cdef uint8_t _compression_factor
     cdef uint32_t _min_range
     cdef uint32_t _max_range
+    cdef uint8_t _tree_height
+    cdef uint32_t _number_of_nodes
 
     cdef uint8_t _seed
 
     cpdef void add(self, object element, bint compress=*) except *
-    cpdef void compress(self, object element) except *
+    cpdef void compress(self) except *
 
-    cpdef void quantile_query(self, uint8_t quantile) except *
-    cpdef void inverse_quantile_query(self, object element) except *
-    cpdef void range_query(self, uint8_t r) except *
+    # cpdef void quantile_query(self, uint8_t quantile) except *
+    # cpdef void inverse_quantile_query(self, object element) except *
+    # cpdef void range_query(self, uint8_t r) except *
     
-    cpdef size_t sizeof(self)
-    cpdef size_t total(self)
+    # cpdef size_t sizeof(self)
+    # cpdef size_t total(self)
 
     cdef uint32_t _hash(self, object element, uint8_t seed)
+    # def get_all_on_level(self, uint8_t level)
+    cdef bint merge_if_needed(self, uint32_t bucket_id)
+    cdef bint qdigest_property(self, size_t counts_sum)
+    cdef uint32_t _bucket_id(self, uint32_t value)
 
--- pdsa/rank/qdigest.pyx
+++ pdsa/rank/qdigest.pyx
@@ -1,6 +1,6 @@
 import cython
 
-from libc.math cimport log, round
+from libc.math cimport floor, log, round
 from libc.stdint cimport uint32_t, uint8_t, UINT32_MAX
 from libc.stdlib cimport rand
 
@@ -20,13 +20,13 @@ cdef class QuantileDigest:
         self._min_range = 0
         self._max_range = 2**32 - 1  # UINT32_MAX since uint32 hash function
         
-        self._tree_height = math.log(self._max_range + 1, 2) + 1  # 32+1=33
+        self._tree_height = <uint8_t>(log(self._max_range + 1) + 1)  # 32+1=33
         self._number_of_nodes = 2**self._tree_height - 1 
         self._total = 0
 
         self._seed = <uint8_t>(rand())
 
-        self._qdigest = list?
+        self._qdigest = {}
         # https://stackoverflow.com/questions/7403966
 
         # cdef size_t index
@@ -38,7 +38,7 @@ cdef class QuantileDigest:
         # self.algorithm = "mmh3_x86_32bit"
         return mmh3_x86_32bit(key, seed)
 
-    cdef size_t _bucket_id(self, uint32_t value):
+    cdef uint32_t _bucket_id(self, uint32_t value):
         # In the full and complete binary tree build for the
         # binary parition of a range [0 .. self._max_range]
         # is the value-th value (from left) in the last level (=height).
@@ -47,48 +47,103 @@ cdef class QuantileDigest:
         # (because exactly self._max_range nodes at the last level).
         return self._number_of_nodes - self._max_range + value
 
-    cdef void add(self, object element, bint compress=False):
-        hashed = self._hash(element, self._seed)
+    cpdef void add(self, object element, bint compress=False) except *:
+        cdef uint32_t hashed = self._hash(element, self._seed)
         if hashed > self._max_range or hashed < self._min_range:
             raise ValueError("Value out of range")
 
-        canonical_bucket_id = self._bucket_id(hashed)
+        cdef uint32_t canonical_bucket_id = self._bucket_id(hashed)
 
         path = []
-        bucket_id = canonical_bucket_id
+        cdef uint32_t bucket_id = canonical_bucket_id
+        cdef uint32_t closest_parent_id_in_digest = 0
         while bucket_id > 0:
-            path.append(bucket_id)
-            bucket_id = bucket_id // 2
-
-        closest_parent_id_in_digest = 0
-        for ind, (bucket_id, _) in enumerate(Digest):
-            if bucket_id in path and bucket_id > closest_parent_id_in_digest:
+            if bucket_id in self._qdigest:
                 closest_parent_id_in_digest = bucket_id
-                bucket_index = ind
+                break
+            bucket_id = bucket_id // 2  # get parent
+
+        assert closest_parent_id_in_digest > 0
 
         if closest_parent_id_in_digest == canonical_bucket_id:
-            Digest[ind][1] += 1  # update counts
+            self._qdigest[canonical_bucket_id] += 1  # update counts
         else:
-            Digest.append((canonical_bucket_id, 1))
-            for bucket_id in path[1:]:
+            self._qdigest[canonical_bucket_id] = 1
+            bucket_id = canonical_bucket_id
+            while bucket_id > 0:
+                bucket_id = bucket_id // 2
                 if bucket_id <= closest_parent_id_in_digest:
-                    # break because path list is sorted on creation
                     break
-
-                Digest.append((bucket_id, 0))
+                self._qdigest[bucket_id] = 0
 
         self._total += 1
 
         if compress:
             self.compress()
 
-    cdef void compress(self):
+    cdef bint qdigest_property(self, size_t counts_sum):
+        cdef size_t boundary_value = <size_t>(floor(self._total / float(self._compression_factor)))
+        if counts_sum <= boundary_value:
+            return False
+        return True
 
+    cdef bint merge_if_needed(self, uint32_t bucket_id):
+        if bucket_id == 1:
+            return False
+
+        cdef uint32_t parent_bucket_id = bucket_id // 2
+        cdef uint32_t sibling_bucket_id = 2 * bucket_id // 2 + 28888
+
+        if bucket_id not in self._qdigest:
+            # NOTE: might be already removed from the QDigest 
+            # after merging because of its sibling
+            return False
+
+        cdef size_t bucket_counts = self._qdigest[bucket_id]
+        cdef size_t bucket_parent_counts = self._qdigest.get(
+            parent_bucket_id, 0)
+        cdef size_t bucket_sibling_counts = self._qdigest.get(
+            sibling_bucket_id, 0)
+
+        cdef size_t counts = bucket_counts + bucket_parent_counts + bucket_sibling_counts
+        if self.qdigest_property(counts):
+            return False
+
+        if parent_bucket_id not in self._qdigest:
+            self._total += 1
+
+        self._qdigest[parent_bucket_id] = counts
+        
+        try:
+            del self._qdigest[bucket_id]
+        except:
+            pass
+        else:
+            self._total -= 1
+
+        try:
+            del self._qdigest[sibling_bucket_id]
+        except:
+            pass
+        else:
+            self._total -= 1
+
+        return True
+
+    def get_all_on_level(self, uint8_t level):
+        # k => 2^{k-1} .. 2^{k} - 1
+        for bucket_id in range(2**(level - 1), 2**level):
+            if bucket_id in self._qdigest:
+                yield bucket_id
+
+    cpdef void compress(self) except *:
+        cdef uint8_t level =  self._tree_height
         while level > 0:
+            buckets = self.get_all_on_level(self, level)
+            for bucket_id in buckets:
+                self._qdigest.merge_if_needed(bucket_id)
+            level -= 1
 
-        - build full binary tree from the qdigest
-        - compress it
-        - save qdigest1 
 
         # Parent of node i is node i / 2, unless i = 1.
         # Node 1 is the root and has no parent.
